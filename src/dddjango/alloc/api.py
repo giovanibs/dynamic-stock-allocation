cdd75982b14ba8110b2623a5ca956bfd52babe7d
Giovani <giovanibulian@hotmail.com>
2024-11-08 03:23:49 -0400

    feat: endpoint for order line allocation

--- src/dddjango/alloc/api.py
+++ src/dddjango/alloc/api.py
@@ -1,7 +1,8 @@
-from typing import List, Optional, Union
-from ninja import Field, NinjaAPI
+from typing import Union
+from ninja import NinjaAPI
 from ninja.schema import Schema
 from allocation.adapters.repository import DjangoRepository
+from allocation.domain import model as domain_models
 from datetime import date
 
 
@@ -13,10 +14,28 @@ class BatchOut(Schema):
     eta: Union[date, None]
 
 
+class OrderLineIn(Schema):
+    order_id: str
+    sku: str
+    qty: int
+
+
+class BatchRef(Schema):
+    batch_reference: str
+
+
 api = NinjaAPI()
 repo = DjangoRepository()
 
 
-@api.get('/{batch_ref}', response=BatchOut)
+@api.get('batches/{batch_ref}', response=BatchOut)
 def get_batch_by_ref(request, batch_ref: str):
-    return repo.get(batch_ref)
+    return 200, repo.get(batch_ref)
+
+
+@api.post('allocate', response = {201: BatchRef})
+def allocate(request, payload: OrderLineIn):
+    order_line = domain_models.OrderLine(**payload.dict())
+    batches = repo.list()
+    batch_ref = domain_models.allocate(order_line, batches)
+    return 201, {'batch_reference': batch_ref}
--- src/tests/e2e/test_django_api.py
+++ src/tests/e2e/test_django_api.py
@@ -2,14 +2,41 @@ from django.test import Client
 import pytest
 from allocation.adapters.repository import DjangoRepository
 from allocation.domain import model as domain_models
+from datetime import date, timedelta
+
+
+@pytest.fixture
+def today():
+    return date.today()
+
+
+@pytest.fixture
+def tomorrow(today):
+    return today + timedelta(days=1)
+
+
+@pytest.fixture
+def later(today):
+    return today + timedelta(days=2)
+
+
+@pytest.fixture
+def base_url():
+    return 'http://127.0.0.1:8000/api/'
+
+
+@pytest.fixture
+def repo():
+    return DjangoRepository()
+
+
+@pytest.fixture
+def client():
+    return Client()
 
 
 @pytest.mark.django_db
-def test_can_retrieve_batch_info():
-    base_url = 'http://127.0.0.1:8000/api/'
-    repo = DjangoRepository()
-    client = Client()
-
+def test_can_retrieve_batch_info(base_url, client, repo):
     batch = domain_models.Batch('ref', 'skew', 10)
     line1 = domain_models.OrderLine('o1', 'skew', 1)
     line2 = domain_models.OrderLine('o2', 'skew', 2)
@@ -17,7 +44,7 @@ def test_can_retrieve_batch_info():
     batch.allocate(line2)
     repo.add(batch)
     
-    response = client.get(base_url + batch.reference)
+    response = client.get(base_url + 'batches/' + batch.reference)
     resp_batch = response.json()
     
     assert response.status_code == 200
@@ -26,3 +53,18 @@ def test_can_retrieve_batch_info():
     assert resp_batch['allocated_qty'] == batch.allocated_qty
     assert resp_batch['available_qty'] == batch.available_qty
     assert resp_batch['eta'] == batch.eta
+
+
+@pytest.mark.django_db
+def test_api_returns_batch_ref_on_allocation(today, tomorrow, later, base_url, client, repo):
+    sku = 'skew'
+    earliest_batch = domain_models.Batch('today', sku, 10, eta=today)
+    in_between_batch = domain_models.Batch('tomorrow', sku, 10, eta=tomorrow)
+    latest_batch = domain_models.Batch('latest', sku, 10, eta=later)
+    repo.add(earliest_batch)
+    repo.add(in_between_batch)
+    repo.add(latest_batch)
+    line = {'order_id': 'o1', 'sku': sku, 'qty': 1}
+    response = client.post(base_url + 'allocate', data = line, content_type = "application/json")
+    assert response.status_code == 201
+    assert response.json()['batch_reference'] == earliest_batch.reference
