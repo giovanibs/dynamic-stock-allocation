a909c402af1a8e8cf965bae4bf84b4c016a55641
Giovani <giovanibulian@hotmail.com>
2024-11-08 16:03:43 -0400

    refactor: removed e2e persistence test since orchestration test does that already

--- src/tests/e2e/test_django_api.py
+++ src/tests/e2e/test_django_api.py
@@ -55,26 +55,6 @@ def test_api_returns_batch_ref_on_allocation(today, tomorrow, later, base_url, c
 
 
 @pytest.mark.django_db
-def test_allocations_are_persisted(today, tomorrow, later, base_url, client, repo):
-    sku = 'skew'
-    earliest_batch = domain_models.Batch('today', sku, 10, eta=today)
-    in_between_batch = domain_models.Batch('tomorrow', sku, 10, eta=tomorrow)
-    latest_batch = domain_models.Batch('latest', sku, 10, eta=later)
-    repo.add(earliest_batch)
-    repo.add(in_between_batch)
-    repo.add(latest_batch)
-    line1 = {'order_id': 'o1', 'sku': sku, 'qty': 10}
-    line2 = {'order_id': 'o2', 'sku': sku, 'qty': 10}
-    response = client.post(base_url + 'allocate', data = line1, content_type = "application/json")
-    assert response.status_code == 201
-    assert response.json()['batch_reference'] == earliest_batch.reference
-
-    response = client.post(base_url + 'allocate', data = line2, content_type = "application/json")
-    assert response.status_code == 201
-    assert response.json()['batch_reference'] == in_between_batch.reference
-
-
-@pytest.mark.django_db
 def test_400_message_for_out_of_stock(base_url, client, repo): 
     batch = domain_models.Batch('batch', 'skew', 10)
     repo.add(batch)
