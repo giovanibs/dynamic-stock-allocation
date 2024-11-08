7579f30c66a4e6b78c8064c5a845359b8709ddf6
Giovani <giovanibulian@hotmail.com>
2024-11-08 15:55:36 -0400

    refactor: using allocate service in the allocate endpoint

--- src/dddjango/alloc/api.py
+++ src/dddjango/alloc/api.py
@@ -6,6 +6,8 @@ from allocation.domain import model as domain_models
 from allocation.domain.exceptions import InvalidSKU, OutOfStock
 from datetime import date
 
+from allocation.orchestration import services
+
 
 class BatchOut(Schema):
     reference: str
@@ -33,6 +35,24 @@ api = NinjaAPI()
 repo = DjangoRepository()
 
 
+class FakeSession:
+    def __init__(self) -> None:
+        self._commited = False
+
+    
+    @property
+    def commited(self):
+        return self._commited
+    
+    
+    def commit(self):
+        self._commited = True
+
+
+def get_session():
+    return FakeSession()
+
+
 @api.get('batches/{batch_ref}', response=BatchOut)
 def get_batch_by_ref(request, batch_ref: str):
     return 200, repo.get(batch_ref)
@@ -41,14 +61,12 @@ def get_batch_by_ref(request, batch_ref: str):
 @api.post('allocate', response = {201: BatchRef, 400: Message})
 def allocate(request, payload: OrderLineIn):
     order_line = domain_models.OrderLine(**payload.dict())
-    batches = repo.list()
-    
+    session = get_session()
     try:
-        batch_ref = domain_models.allocate(order_line, batches)
+        batch_ref = services.allocate(order_line, repo, session)
     except OutOfStock:
         return 400, {'message': 'OutOfStock'}
     except InvalidSKU:
         return 400, {'message': 'InvalidSKU'}
         
-    repo.update(next(b for b in batches if b.reference == batch_ref))
     return 201, {'batch_reference': batch_ref}
