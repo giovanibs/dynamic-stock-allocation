99bad5493c89fc136a51281cec974ab8419e7050
Giovani <giovanibulian@hotmail.com>
2024-11-09 19:13:19 -0400

    test: add more integrity tests for DjangoUoW

--- src/tests/integration/test_uow.py
+++ src/tests/integration/test_uow.py
@@ -20,6 +20,36 @@ def test_uow_can_retrieve_a_batch_and_allocate_to_it():
     assert batch_ref == 'batch'
 
 
+@pytest.mark.django_db(transaction=True)
+def test_uow_does_not_commit_implicitly():
+    uow = DjangoUoW()
+
+    with uow:
+        uow.batches.add(domain_models.Batch('batch', 'skew', 10))
+
+    with pytest.raises(dj_models.Batch.DoesNotExist):
+        dj_models.Batch.objects.get(reference='batch')
+
+
+@pytest.mark.django_db(transaction=True)
+def test_uow_rollbacks_on_error():
+    insert_batch('batch', 'skew', 10)
+
+    uow = DjangoUoW()
+    try:
+        with uow:
+            batch = uow.batches.get(reference='batch')
+            line = domain_models.OrderLine('o1', 'skew', 1)
+            batch.allocate(line)
+            invalid_line = domain_models.OrderLine('o1', 'skewer', 1)
+            batch.allocate(invalid_line)
+            uow.commit()
+    except:
+        pass
+    
+    assert dj_models.Batch.objects.get(reference='batch').to_domain().allocated_qty == 0
+
+
 def insert_batch(reference, sku, purchased_qty, eta=None):
     dj_models.Batch.objects.create(
         reference=reference,
@@ -29,5 +59,9 @@ def insert_batch(reference, sku, purchased_qty, eta=None):
     )
 
 def get_allocated_batch_ref(order_id, sku):
-    allocation = dj_models.Allocation.objects.get(order_id=order_id, sku=sku)
+    try:
+        allocation = dj_models.Allocation.objects.get(order_id=order_id, sku=sku)
+    except dj_models.Allocation.DoesNotExist:
+        return None
+    
     return allocation.batch.reference
