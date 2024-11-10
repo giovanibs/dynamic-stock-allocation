7e3247167162aadd6ace56f8312770cc866a6bea
Giovani <giovanibulian@hotmail.com>
2024-11-10 04:42:37 -0400

    feat(wip): Implementing Product aggregate step 3 (django model)

--- src/dddjango/alloc/models.py
+++ src/dddjango/alloc/models.py
@@ -3,9 +3,23 @@ from django.db import models
 from allocation.domain import model as domain_model
 
 
+class Product(models.Model):
+    sku = models.CharField(max_length=255, primary_key=True)
+
+    class Meta:
+        app_label = 'alloc'
+    
+    
+    def to_domain(self) -> domain_model.Product:
+        return domain_model.Product(
+            self.sku,
+            [batch.to_domain() for batch in self.batches.all()]
+        )
+
+
 class Batch(models.Model):
     reference = models.CharField(max_length=255, primary_key=True)
-    sku = models.CharField(max_length=255)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE, related_name='batches')
     purchased_qty = models.IntegerField()
     eta = models.DateField(blank=True, null=True)
 
@@ -16,7 +30,7 @@ class Batch(models.Model):
     def to_domain(self) -> domain_model.Batch:
 
         domain_batch = domain_model.Batch(
-            self.reference, self.sku, self.purchased_qty, self.eta
+            self.reference, self.product.sku, self.purchased_qty, self.eta
         )
         for line in Allocation.allocations_to_domain(self):
             domain_batch.allocate(line)
@@ -25,8 +39,8 @@ class Batch(models.Model):
 
 
 class Allocation(models.Model):
-    batch = models.ForeignKey(Batch, on_delete=models.CASCADE, null=True)
-    order_id = models.CharField(max_length=255, unique=True)
+    batch = models.ForeignKey(Batch, on_delete=models.CASCADE, null=True, related_name='allocations')
+    order_id = models.CharField(max_length=255)
     sku = models.CharField(max_length=255)
     qty = models.IntegerField()
 
@@ -38,5 +52,5 @@ class Allocation(models.Model):
     def allocations_to_domain(Batch) -> Set[domain_model.OrderLine]:
         return {
             domain_model.OrderLine(line.order_id, line.sku, line.qty)
-            for line in Batch.allocation_set.all()
+            for line in Batch.allocations.all()
         }
