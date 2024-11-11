6701b40bac6162f06419a4fe02309ad6c2ee5028
Giovani <giovanibulian@hotmail.com>
2024-11-10 21:44:46 -0400

    feat(wip): Implementing Product aggregate step 5 (django uow)
    -  retrieve a product and add a batch
    - allocate line

--- src/tests/integration/test_product_uow.py
+++ src/tests/integration/test_product_uow.py
@@ -14,15 +14,29 @@ def test_uow_can_retrieve_a_product_and_add_a_batch():
         product.add_batch('batch', 'skew', 10)
         uow.commit()
 
-    assert retrieve_batch_from_db('batch') is not None
+    assert retrieve_batch_from_db('batch').reference == 'batch'
 
 
-def insert_product_into_db(sku):
+@pytest.mark.django_db(transaction=True)
+def test_uow_can_allocate_a_line():
+    insert_product_into_db('skew')
+    uow = DjangoProductUoW()
+
+    with uow:
+        product = uow.products.get('skew')
+        product.add_batch('batch', 'skew', 10)
+        product.allocate('o1', 'skew', 1)
+        uow.commit()
+
+    assert retrieve_batch_from_db('batch').allocations[0].order_id == 'o1'
+
+
+def insert_product_into_db(sku) -> None:
     django_models.Product.objects.create(sku=sku)
 
 
-def retrieve_batch_from_db(reference):
+def retrieve_batch_from_db(reference) -> domain_models.Batch | None:
     try:
-        return django_models.Batch.objects.get(reference=reference)
+        return django_models.Batch.objects.get(reference=reference).to_domain()
     except django_models.Batch.DoesNotExist:
         return None
