fcebbdc4b66f7c7f2e89b0569d434158d8006491
Giovani <giovanibulian@hotmail.com>
2024-11-11 22:06:11 -0400

    refactor: removed unnecessary pytest fixture

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,12 +1,5 @@
 import pytest
-from django.core.management import call_command
 from datetime import date, timedelta
-
-
-@pytest.fixture(scope='session')
-def django_db_setup(django_db_blocker):
-    with django_db_blocker.unblock():
-        call_command('migrate', verbosity=0)
 
 
 @pytest.fixture(scope='session')
