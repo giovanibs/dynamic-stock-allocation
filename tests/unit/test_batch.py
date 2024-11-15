cd6619390366ff5677bfa3ec6039dccb0e8573d2
Giovani <giovanibulian@hotmail.com>
2024-11-15 19:15:48 -0400

    refactor: organized Product and Batches tests into classes

--- tests/unit/test_batch.py
+++ tests/unit/test_batch.py
@@ -4,6 +4,69 @@ from allocation.domain.exceptions import (
 import pytest
 
 
+class TestBatchAllocation:
+    
+    def test_allocating_reduces_available_quantity(self):
+        initial_available_qty = 10
+        order_qty = 5
+        
+        batch, order_line = create_batch_and_order_line(
+            initial_available_qty, order_qty
+        )
+        batch.allocate(order_line)
+        assert batch.available_qty == (initial_available_qty - order_qty)
+
+
+    def test_cannot_overallocate(self):
+        batch, order_line = create_batch_and_order_line(10, 15)
+
+        with pytest.raises(CannotOverallocateError):
+            batch.allocate(order_line)
+
+
+    def test_can_allocate_if_available_greater_than_required(self):
+        batch, order_line = create_batch_and_order_line(10, 5)
+        batch.allocate(order_line)
+
+
+    def test_can_allocate_if_available_equal_to_required(self):
+        batch, order_line = create_batch_and_order_line(10, 10)
+        batch.allocate(order_line)
+
+
+    def test_cannot_allocate_if_skus_dont_match(self, today):
+        batch = Batch('batch_000', 'sku_000', 10, today)
+        order_line = OrderLine('order_000', 'sku_001', 5)
+
+        with pytest.raises(SKUsDontMatchError):
+            batch.allocate(order_line)
+
+
+    def test_allocation_is_idempotent(self):
+        batch, order_line = create_batch_and_order_line(10, 5)
+        batch.allocate(order_line)
+        batch.allocate(order_line)
+
+        assert batch.allocated_qty == order_line.qty
+
+
+class TestBatchDeallocation:
+    
+    def test_can_deallocate_allocated_order_line(self):
+        batch, order_line = create_batch_and_order_line(10, 5)
+        batch.allocate(order_line)
+        batch.deallocate(order_line)
+        assert order_line not in batch._allocations
+        assert batch.available_qty == 10
+
+
+    def test_cannot_deallocate_unallocated_line(self):
+        batch, order_line = create_batch_and_order_line(10, 5)
+        
+        with pytest.raises(LineIsNotAllocatedError):
+            batch.deallocate(order_line)
+
+
 def create_batch_and_order_line(batch_qty, order_line_qty):
     dummy_sku = 'product_000'
     
@@ -11,62 +74,3 @@ def create_batch_and_order_line(batch_qty, order_line_qty):
         Batch('batch_000', dummy_sku, batch_qty),
         OrderLine('order_000', dummy_sku, order_line_qty)
     )
-
-
-def test_allocating_reduces_available_quantity():
-    initial_available_qty = 10
-    order_qty = 5
-    
-    batch, order_line = create_batch_and_order_line(
-        initial_available_qty, order_qty
-    )
-    batch.allocate(order_line)
-    assert batch.available_qty == (initial_available_qty - order_qty)
-
-
-def test_cannot_overallocate():
-    batch, order_line = create_batch_and_order_line(10, 15)
-
-    with pytest.raises(CannotOverallocateError):
-        batch.allocate(order_line)
-
-
-def test_can_allocate_if_available_greater_than_required():
-    batch, order_line = create_batch_and_order_line(10, 5)
-    batch.allocate(order_line)
-
-
-def test_can_allocate_if_available_equal_to_required():
-    batch, order_line = create_batch_and_order_line(10, 10)
-    batch.allocate(order_line)
-
-
-def test_cannot_allocate_if_skus_dont_match(today):
-    batch = Batch('batch_000', 'sku_000', 10, today)
-    order_line = OrderLine('order_000', 'sku_001', 5)
-
-    with pytest.raises(SKUsDontMatchError):
-        batch.allocate(order_line)
-
-
-def test_can_deallocate_allocated_order_line():
-    batch, order_line = create_batch_and_order_line(10, 5)
-    batch.allocate(order_line)
-    batch.deallocate(order_line)
-    assert order_line not in batch._allocations
-    assert batch.available_qty == 10
-
-
-def test_cannot_deallocate_unallocated_line():
-    batch, order_line = create_batch_and_order_line(10, 5)
-    
-    with pytest.raises(LineIsNotAllocatedError):
-        batch.deallocate(order_line)
-
-
-def test_allocation_is_idempotent():
-    batch, order_line = create_batch_and_order_line(10, 5)
-    batch.allocate(order_line)
-    batch.allocate(order_line)
-
-    assert batch.allocated_qty == order_line.qty
--- tests/unit/test_product.py
+++ tests/unit/test_product.py
@@ -4,111 +4,119 @@ from allocation.domain.exceptions import InvalidSKU, LineIsNotAllocatedError
 import pytest
 
 
-def test_prefers_current_stock_batches_to_shipments(tomorrow):
-    sku = 'skew'
-    in_stock_batch = Batch('in-stock', sku, 10, eta=None)
-    shipping_batch = Batch('shipping', sku, 10, eta=tomorrow)
-    product = Product(sku, [in_stock_batch, shipping_batch])
-    line = ('order_ref', sku, 5)
-    product.allocate(*line)
-
-    assert in_stock_batch.available_qty == 5
-    assert shipping_batch.available_qty == 10
-
-
-def test_prefers_earlier_batches(today, tomorrow, later):
-    sku = 'skew'
-    earliest_batch = Batch('today', sku, 10, eta=today)
-    in_between_batch = Batch('tomorrow', sku, 10, eta=tomorrow)
-    latest_batch = Batch('latest', sku, 10, eta=later)
-    line = ('order_ref', sku, 5)
-    product = Product(sku, [earliest_batch, in_between_batch, latest_batch])
-    product.allocate(*line)
-
-    assert earliest_batch.available_qty == 5
-    assert in_between_batch.available_qty == 10
-    assert latest_batch.available_qty == 10
-
-
-def test_allocation_returns_allocated_batch_ref(today, tomorrow, later):
-    sku = 'skew'
-    earliest_batch = Batch('today', sku, 10, eta=today)
-    in_between_batch = Batch('tomorrow', sku, 10, eta=tomorrow)
-    latest_batch = Batch('latest', sku, 10, eta=later)
-    line = ('order_ref', sku, 5)
-    product = Product(sku, [earliest_batch, in_between_batch, latest_batch])
-    batch_ref = product.allocate(*line)
-
-    assert batch_ref == earliest_batch.ref
-
-
-def test_deallocate_returns_batch_ref():
-    sku = 'skew'
-    batch_with_the_line = Batch('it_is_me', sku, 10)
-    batch_without_the_line = Batch('it_is_not_me', sku, 10)
-    line = ('o1', sku, 1)
-    batch_with_the_line.allocate(OrderLine(*line))
-    product = Product(sku, [batch_with_the_line, batch_without_the_line])
-    batch_ref = product.deallocate(*line)
-
-    assert batch_ref == batch_with_the_line.ref
-
-
-def test_deallocate_raises_error_for_invalid_sku():
-    batch = Batch('batch', 'skew', 10)
-    other_batch = Batch('other_batch', 'skew', 10)
-    product = Product('skew', [batch, other_batch])
-    line_with_invalid_sku = ('o1', 'invalid_skew', 1)
+class TestProductAllocation:
     
-    with pytest.raises(InvalidSKU):
-        product.deallocate(*line_with_invalid_sku)
+    def test_prefers_current_stock_batches_to_shipments(self, tomorrow):
+        sku = 'skew'
+        in_stock_batch = Batch('in-stock', sku, 10, eta=None)
+        shipping_batch = Batch('shipping', sku, 10, eta=tomorrow)
+        product = Product(sku, [in_stock_batch, shipping_batch])
+        line = ('order_ref', sku, 5)
+        product.allocate(*line)
+
+        assert in_stock_batch.available_qty == 5
+        assert shipping_batch.available_qty == 10
 
 
-def test_deallocate_raises_error_for_not_allocated_line():
-    batch_without_the_line = Batch('it_is_not_me', 'skew', 10)
-    other_batch_without_the_line = Batch('it_is_not_me_either', 'skew', 10)
-    product = Product('skew', [batch_without_the_line, other_batch_without_the_line])
-    line_not_allocated = ('o1', 'skew', 1)
+    def test_prefers_earlier_batches(self, today, tomorrow, later):
+        sku = 'skew'
+        earliest_batch = Batch('today', sku, 10, eta=today)
+        in_between_batch = Batch('tomorrow', sku, 10, eta=tomorrow)
+        latest_batch = Batch('latest', sku, 10, eta=later)
+        line = ('order_ref', sku, 5)
+        product = Product(sku, [earliest_batch, in_between_batch, latest_batch])
+        product.allocate(*line)
+
+        assert earliest_batch.available_qty == 5
+        assert in_between_batch.available_qty == 10
+        assert latest_batch.available_qty == 10
+
+
+    def test_allocation_returns_allocated_batch_ref(self, today, tomorrow, later):
+        sku = 'skew'
+        earliest_batch = Batch('today', sku, 10, eta=today)
+        in_between_batch = Batch('tomorrow', sku, 10, eta=tomorrow)
+        latest_batch = Batch('latest', sku, 10, eta=later)
+        line = ('order_ref', sku, 5)
+        product = Product(sku, [earliest_batch, in_between_batch, latest_batch])
+        batch_ref = product.allocate(*line)
+
+        assert batch_ref == earliest_batch.ref
+
+
+class TestProductDeallocation:
     
-    with pytest.raises(LineIsNotAllocatedError):
-        product.deallocate(*line_not_allocated)
+    def test_deallocate_returns_batch_ref(self):
+        sku = 'skew'
+        batch_with_the_line = Batch('it_is_me', sku, 10)
+        batch_without_the_line = Batch('it_is_not_me', sku, 10)
+        line = ('o1', sku, 1)
+        batch_with_the_line.allocate(OrderLine(*line))
+        product = Product(sku, [batch_with_the_line, batch_without_the_line])
+        batch_ref = product.deallocate(*line)
+
+        assert batch_ref == batch_with_the_line.ref
 
 
-def test_can_add_valid_batch():
-    product = Product('sku')
-    batch = ('batch', 'sku', 10)
-    product.add_batch(*batch)
-    assert 'batch' in {b.ref for b in product.batches}
+    def test_deallocate_raises_error_for_invalid_sku(self):
+        batch = Batch('batch', 'skew', 10)
+        other_batch = Batch('other_batch', 'skew', 10)
+        product = Product('skew', [batch, other_batch])
+        line_with_invalid_sku = ('o1', 'invalid_skew', 1)
+        
+        with pytest.raises(InvalidSKU):
+            product.deallocate(*line_with_invalid_sku)
 
 
-def test_cannot_add_batch_with_different_sku():
-    product = Product('sku')
-    batch = ('batch', 'other_sku', 10)
+    def test_deallocate_raises_error_for_not_allocated_line(self):
+        batch_without_the_line = Batch('it_is_not_me', 'skew', 10)
+        other_batch_without_the_line = Batch('it_is_not_me_either', 'skew', 10)
+        product = Product('skew', [batch_without_the_line, other_batch_without_the_line])
+        line_not_allocated = ('o1', 'skew', 1)
+        
+        with pytest.raises(LineIsNotAllocatedError):
+            product.deallocate(*line_not_allocated)
+
+
+class TestProductInstantiation:
     
-    with pytest.raises(InvalidSKU):
+    def test_can_add_valid_batch(self):
+        product = Product('sku')
+        batch = ('batch', 'sku', 10)
         product.add_batch(*batch)
+        assert 'batch' in {b.ref for b in product.batches}
 
 
-def test_can_instantiate_product_with_batches():
-    batch = Batch('batch', 'skew', 10)
-    other_batch = Batch('other_batch', 'skew', 10)
-    product = Product('skew', [batch, other_batch])
-    assert batch.ref in {b.ref for b in product.batches}
-    assert other_batch.ref in {b.ref for b in product.batches}
+    def test_cannot_add_batch_with_different_sku(self):
+        product = Product('sku')
+        batch = ('batch', 'other_sku', 10)
+        
+        with pytest.raises(InvalidSKU):
+            product.add_batch(*batch)
 
 
-def test_cannot_instantiate_product_with_invalid_batch():
-    batch = Batch('batch', 'other_skew', 10)
+    def test_can_instantiate_product_with_batches(self):
+        batch = Batch('batch', 'skew', 10)
+        other_batch = Batch('other_batch', 'skew', 10)
+        product = Product('skew', [batch, other_batch])
+        assert batch.ref in {b.ref for b in product.batches}
+        assert other_batch.ref in {b.ref for b in product.batches}
+
+
+    def test_cannot_instantiate_product_with_invalid_batch(self):
+        batch = Batch('batch', 'other_skew', 10)
+        
+        with pytest.raises(InvalidSKU):
+            Product('skew', [batch])
+
+
+class TestProductEventRaising:
     
-    with pytest.raises(InvalidSKU):
-        Product('skew', [batch])
+    def test_records_out_of_stock_event_if_cannot_allocate(self):
+        sku = 'skew'
+        product = Product(sku)
+        product.add_batch('batch', sku, 1)
+        allocation = product.allocate('o1', sku, 2)
 
-
-def test_records_out_of_stock_event_if_cannot_allocate():
-    sku = 'skew'
-    product = Product(sku)
-    product.add_batch('batch', sku, 1)
-    allocation = product.allocate('o1', sku, 2)
-
-    assert product.messages[0] == events.OutOfStock(sku)
-    assert allocation is None
+        assert product.messages[-1] == events.OutOfStock(sku)
+        assert allocation is None
